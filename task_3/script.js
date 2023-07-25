c5142da7f3b3ad03abf96ed86f7589f9efea0df0
Leslie Saliba <lesliesaliba@gmail.com>
2023-07-25 15:53:31 +0300

    Done with task 3

---
+++ task_3/script.js
@@ -0,0 +1,32 @@
+console.log('Test')
+
+const num1 = 3;
+let num2 = 7;
+
+if (num1 >= num2) {
+    console.log('Hello');
+}
+else {
+    console.log('Bye')
+}
+
+num2 = 1
+
+if (num1 >= num2) {
+    console.log('Hello');
+}
+else {
+    console.log('Bye')
+}
+
+
+let canPass = true
+if (canPass) {
+    if (num1 >= num2) {
+        console.log('Hello');
+    }
+    else {
+        console.log('Bye')
+    }
+}
+else { console.log('Try again') }
